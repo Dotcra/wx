2b074372d92bc04ecb3399d186d1498a17562d97
Dot <dotcra@gmail.com>
2017-02-25 17:03:50 +0800

    ok

--- wx/index.php
+++ wx/index.php
@@ -3,7 +3,7 @@ $sig = $_GET["signature"];
 $timestamp = $_GET["timestamp"];
 $nonce = $_GET["nonce"];
 $ranstr = $_GET["echostr"];
-$token = "dot";
+$token = "sb165g78315j8kqb1z5";
 
 $a = array($token,$timestamp,$nonce);
 sort($a, SORT_STRING);
@@ -12,11 +12,5 @@ if($signature == $sig){
 	echo $ranstr;
 }
 else{
-	echo $ranstr."\n";
 	echo "fuckyou\n";
 }
-
-$fp = fopen("log.html", "a+");
-fwrite($fp, $sig ." ". $timestamp ." ". $nonce ." ". $ranstr ." ". $token. "\n");
-fwrite($fp, $signature ."\n");
-fclose($fp);
