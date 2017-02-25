fff6c1da91756bc5355368ea54c7bf5c01f8e5c4
Dot <dotcra@gmail.com>
2017-02-25 17:40:07 +0800

    ignore wx/

--- wx/index.php
+++ wx/index.php
@@ -3,7 +3,7 @@ $sig = $_GET["signature"];
 $timestamp = $_GET["timestamp"];
 $nonce = $_GET["nonce"];
 $ranstr = $_GET["echostr"];
-$token = "sb165g78315j8kqb1z5";
+$token = "sb165g78315j8kqb1z5t";
 
 $a = array($token,$timestamp,$nonce);
 sort($a, SORT_STRING);
