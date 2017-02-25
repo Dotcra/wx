1d77cac6d61868afc07ce2d7e370651a9fbe0888
Dot <dotcra@gmail.com>
2017-02-25 16:04:02 +0800

    nth

--- wx/index.php
+++ wx/index.php
@@ -4,7 +4,7 @@ $timestamp = $_GET["timestamp"];
 $nonce = $_GET["nonce"];
 $ranstr = $_GET["echostr"];
 
-$token = "sag78315j8kqb1z";
+$token = "dot";
 
 $a = array($token,$timestamp,$nonce);
 $signature = sha1(implode(sort($a)));
