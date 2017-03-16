d01371a037de286266b76b838fd904e2b44a9b03
Dot <dotcra@gmail.com>
2017-03-16 16:43:40 +0800

    adjust greet so that wx and wxbeta can both attch

--- wx/greet.php
+++ wx/greet.php
@@ -5,28 +5,30 @@
  */
 
 class greet{
-	private $timestamp;
-	private $nonce;
-	private $sig0;
-	private $str;
-	private $token;
-
 	function __construct(){
 		if(isset($_GET["echostr"])){
-			$arr = json_decode(file_get_contents('key.json'), 1);
-			$this->token = $arr['wx'][2];
-			//$this->token = $arr['wxbeta'][2];
-			$this->timestamp = $_GET["timestamp"];
-			$this->nonce = $_GET["nonce"];
-			$this->sig0 = $_GET["signature"];
-			$this->str = $_GET["echostr"];
-			$a = array($this->timestamp,$this->nonce,$this->token);
+			$arr = json_decode(file_get_contents('sec/key.json'), 1);
+
+			$token = $arr['wx']['token'];
+			$token_beta = $arr['wxbeta']['token'];
+
+			$timestamp = $_GET["timestamp"];
+			$nonce = $_GET["nonce"];
+			$sig0 = $_GET["signature"];
+			$str = $_GET["echostr"];
+
+			$a = array($timestamp,$nonce,$token);
 			sort($a, SORT_STRING);
 			$sig = sha1(implode($a));
-			if($sig == $this->sig0)
-				echo $this->str;
+
+			$a_beta = array($timestamp,$nonce,$token_beta);
+			sort($a_beta, SORT_STRING);
+			$sig_beta = sha1(implode($a_beta));
+
+			if($sig == $sig0 || $sig_beta == $sig0)
+				echo $str;
 			else
-				echo "get out\n";
+				echo "fuck off\n";
 			exit;
 		}
 	}
