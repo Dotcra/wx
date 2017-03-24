8c2fca26f3a0eaceeca63716a7fe7ab7cdb8b00f
Dot <dotcra@gmail.com>
2017-03-24 18:04:02 +0800

    auto commit by dotr

--- wx/a.php
+++ wx/a.php
@@ -5,7 +5,22 @@
  * @todo
  */
 
-$a = new mysqli('qdm189698650.my3w.com', 'qdm189698650', '19860625', 'qdm189698650_db');
-if ($a->connect_error) echo $a->connect_error;
-else echo 'ok';
+//$a = new mysqli('qdm189698650.my3w.com', 'qdm189698650', '19860625', 'qdm189698650_db');
+$a = new mysqli('localhost', 'root', 'dot', 'grind');
+if ($a->connect_error) exit($a->connect_error);
+//else echo 'ok';
+
+$limit = 5;
+$sql = "select * from dodo order by `time` limit $limit";
+
+$res = $a->query($sql);
+$b = $res->fetch_all(MYSQLI_ASSOC);
+//$isay = '';
+foreach ( $b as $k => $v) {
+	$isay .= $v['time'].' '.$v['act'].' '.$v['comment']."\n";
+}
+
+$res->free();
 $a->close();
+
+echo $isay;
